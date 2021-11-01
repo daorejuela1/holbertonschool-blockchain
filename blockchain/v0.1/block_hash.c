1c9945133718f0e20daa8ed212fc410be15e4454
daorejuela1 <daorejuela1@outlook.com>
2021-11-01 22:02:04 +0000

    Used info block instead of data to get the hash

--- blockchain/v0.1/block_hash.c
+++ blockchain/v0.1/block_hash.c
@@ -27,7 +27,9 @@
 uint8_t *block_hash(block_t const *block,
 		uint8_t hash_buf[SHA256_DIGEST_LENGTH])
 {
-	size_t len =  block->data.len;
+	size_t len =  sizeof(block->info) + block->data.len;
 
-	return (sha256((int8_t const *)&(block->data), len, hash_buf));
+	if (!block || !hash_buf)
+		return (NULL);
+	return (sha256((int8_t const *)block, len, hash_buf));
 }
