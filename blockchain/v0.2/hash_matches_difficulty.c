24e10c0213159bfffb618089fb0593ae570f26d5
daorejuela1 <daorejuela1@outlook.com>
2022-03-17 06:37:09 +0000

    Fixed case when finding 1

--- blockchain/v0.2/hash_matches_difficulty.c
+++ blockchain/v0.2/hash_matches_difficulty.c
@@ -1,27 +1,5 @@
 #include "blockchain.h"
 
-/**
- * get_difficulty - computers number of leading bits in hash
- * @hash: hash buffer
- * Return: computed difficulty
- */
-uint32_t get_difficulty(uint8_t const hash[SHA256_DIGEST_LENGTH])
-{
-	uint8_t *ptr = (uint8_t *)hash;
-	uint32_t difficulty = 0;
-	int i;
-
-	for (; ptr < hash + SHA256_DIGEST_LENGTH; ptr++)
-	{
-		for (i = 7; i >= 0; i--)
-		{
-			if ((*ptr >> i) & 1)
-				return (difficulty);
-			difficulty++;
-		}
-	}
-	return (difficulty);
-}
 
 /**
  * hash_matches_difficulty - determines if hash matches difficulty
@@ -30,9 +8,23 @@ uint32_t get_difficulty(uint8_t const hash[SHA256_DIGEST_LENGTH])
  * Return: 1 if matches else 0
  */
 int hash_matches_difficulty(uint8_t const hash[SHA256_DIGEST_LENGTH],
-	uint32_t difficulty)
+		uint32_t difficulty)
 {
+	uint8_t *ptr = (uint8_t *)hash;
+	uint32_t difficulty_match = 0;
+	int i;
+
 	if (!hash)
 		return (0);
-	return (get_difficulty(hash) >= difficulty);
+	while (ptr < hash + SHA256_DIGEST_LENGTH)
+	{
+		for (i = 7; i >= 0; i--)
+		{
+			if ((*ptr >> i) & 1)
+				return (difficulty_match == difficulty);
+			difficulty_match++;
+		}
+		ptr++;
+	}
+	return (difficulty_match == difficulty);
 }
