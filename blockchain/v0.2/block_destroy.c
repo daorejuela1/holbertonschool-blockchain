836284b1a9089e9a745eea188f5f401ca48bdcb8
daorejuela1 <daorejuela1@outlook.com>
2022-03-17 06:17:00 +0000

    Added base files

---
+++ blockchain/v0.2/block_destroy.c
@@ -0,0 +1,30 @@
+/*
+ *==========================================================================
+ *
+ *       Filename:  block_destroy.c
+ *
+ *    Description:  Destroy a block for a blockchain structure
+ *
+ *        Version:  1.0
+ *        Created:  11/01/2021 09:11:08 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  David Orejuela
+ *   Organization:  Holberton
+ *
+ *=========================================================================
+ */
+#include "blockchain.h"
+
+/**
+ * block_destroy - destroys a block for a blockchain structure
+ *
+ * @block: block in the blockchain to destroy
+ *
+ * Return: Nothing
+ */
+void block_destroy(block_t *block)
+{
+	free(block);
+}
---
+++ blockchain/v0.2/blockchain_destroy.c
@@ -0,0 +1,33 @@
+/*
+ *==========================================================================
+ *
+ *       Filename:  block_destroy.c
+ *
+ *    Description:  destroys a blockchain structure
+ *
+ *        Version:  1.0
+ *        Created:  11/01/2021 09:11:08 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  David Orejuela
+ *   Organization:  Holberton
+ *
+ *=========================================================================
+ */
+#include "blockchain.h"
+
+/**
+ * blockchain_destroy - destroys a blockchain
+ *
+ * @blockchain: blockchain to destroy
+ *
+ * Return: Nothing
+ */
+void blockchain_destroy(blockchain_t *blockchain)
+{
+	if (!blockchain)
+		return;
+	llist_destroy(blockchain->chain, 1, NULL);
+	free(blockchain);
+}
