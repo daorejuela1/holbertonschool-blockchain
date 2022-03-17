913ebf9aeb4f3dc0417dc412ad536142177406f8
daorejuela1 <daorejuela1@outlook.com>
2022-03-17 06:32:03 +0000

    Test methods

--- blockchain/v0.2/hash_matches_difficulty.c
+++ blockchain/v0.2/hash_matches_difficulty.c
@@ -1,4 +1,28 @@
 #include "blockchain.h"
+
+/**
+ * get_difficulty - computers number of leading bits in hash
+ * @hash: hash buffer
+ * Return: computed difficulty
+ */
+uint32_t get_difficulty(uint8_t const hash[SHA256_DIGEST_LENGTH])
+{
+	uint8_t *ptr = (uint8_t *)hash;
+	uint32_t difficulty = 0;
+	int i;
+
+	for (; ptr < hash + SHA256_DIGEST_LENGTH; ptr++)
+	{
+		for (i = 7; i >= 0; i--)
+		{
+			if ((*ptr >> i) & 1)
+				return (difficulty);
+			difficulty++;
+		}
+	}
+	return (difficulty);
+}
+
 /**
  * hash_matches_difficulty - determines if hash matches difficulty
  * @hash: hash buffer
@@ -6,23 +30,9 @@
  * Return: 1 if matches else 0
  */
 int hash_matches_difficulty(uint8_t const hash[SHA256_DIGEST_LENGTH],
-		uint32_t difficulty)
+	uint32_t difficulty)
 {
-	uint8_t *ptr = (uint8_t *)hash;
-	uint32_t difficulty_match = 0;
-	int i;
-
 	if (!hash)
 		return (0);
-	while (ptr < hash + SHA256_DIGEST_LENGTH)
-	{
-		for (i = 7; i >= 0; i--)
-		{
-			if ((*ptr >> i) & 1)
-				break;
-			difficulty_match++;
-		}
-		ptr++;
-	}
-	return (difficulty_match >= difficulty);
+	return (get_difficulty(hash) >= difficulty);
 }
