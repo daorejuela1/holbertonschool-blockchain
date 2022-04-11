d0d29559dea8df1ab563c312fa85ab8f8713a617
daorejuela1 <daorejuela1@outlook.com>
2022-04-11 02:35:14 +0000

    Added task 10

--- blockchain/v0.3/block_create.c
+++ blockchain/v0.3/block_create.c
@@ -18,29 +18,25 @@
 #include "blockchain.h"
 
 /**
- * block_create - creates a new block for a blockchain structure
- *
- * @prev: Previous block in the blockchain
- * @data: data to insert to the block
- * @data_len: data len
- *
- * Return: created block_t or NULL on failure
+ * block_create - creates a new block in the sequence
+ * @prev: pointer to previous block
+ * @data: pointer of data to duplicate
+ * @data_len: length of data
+ * Return: pointer to new block or NULL
  */
 block_t *block_create(block_t const *prev, int8_t const *data,
-		uint32_t data_len)
+	uint32_t data_len)
 {
-	block_t *new_block = NULL;
-	int max_data = 0;
+	block_t *block = calloc(1, sizeof(*block));
+	llist_t *transactions = llist_create(MT_SUPPORT_FALSE);
 
-	max_data = data_len > BLOCKCHAIN_DATA_MAX ? BLOCKCHAIN_DATA_MAX : data_len;
-	new_block = calloc(1, sizeof(*new_block));
-	if (!new_block)
-		return (NULL);
-	new_block->info.timestamp = (time(NULL));
-	memcpy(&(new_block->data.buffer), data, max_data);
-	new_block->data.len = max_data;
-	new_block->info.index = prev->info.index + 1;
-	memcpy(&(new_block->info.prev_hash), prev->hash, SHA256_DIGEST_LENGTH);
-
-	return (new_block);
+	if (!block || !transactions)
+		return (free(block), llist_destroy(transactions, 0, NULL), NULL);
+	memcpy(&(block->data.buffer), data, MIN(data_len, BLOCKCHAIN_DATA_MAX));
+	block->data.len = MIN(data_len, BLOCKCHAIN_DATA_MAX);
+	block->info.index = prev->info.index + 1;
+	block->info.timestamp = time(NULL);
+	block->transactions = transactions;
+	memcpy(&(block->info.prev_hash), prev->hash, SHA256_DIGEST_LENGTH);
+	return (block);
 }
