330a8ee20c170c052a5c9df17b0ad4f192318d5f
daorejuela1 <daorejuela1@outlook.com>
2022-04-11 02:42:25 +0000

    updated blockchain create and destroy

--- blockchain/v0.3/blockchain_create.c
+++ blockchain/v0.3/blockchain_create.c
@@ -18,49 +18,40 @@
 
 #include "blockchain.h"
 
+#define GENESIS_TIMESTAMP 1537578000
+#define GENESIS_DATA "Holberton School"
+#define GENESIS_DATA_LEN 16
+#define GENESIS_HASH "\xc5\x2c\x26\xc8\xb5\x46\x16\x39\x63\x5d\x8e\xdf\x2a\x97\xd4\x8d\x0c\x8e\x00\x09\xc8\x17\xf2\xb1\xd3\xd7\xff\x2f\x04\x51\x58\x03"
+
 /**
- * blockchain_create - Creates a new blockchain structure
- *
- * Return: The blockchain structure with the genesis block
+ * blockchain_create - creates a genesis blockchain
+ * Return: pointer to new blockchain or NULL on error
  */
 blockchain_t *blockchain_create(void)
 {
-	block_t *block = NULL;
-	blockchain_t *blockchain = NULL;
+	blockchain_t *chain = calloc(1, sizeof(*chain));
+	block_t *block = calloc(1, sizeof(*block));
 	llist_t *list = llist_create(MT_SUPPORT_TRUE);
-	block_t const _genesis = {
-		{ /* info */
-			0 /* index */,
-			0, /* difficulty */
-			1537578000, /* timestamp */
-			0, /* nonce */
-			{0} /* prev_hash */
-		},
-		{ /* data */
-			"Holberton School", /* buffer */
-			16 /* len */
-		},
-		"\xc5\x2c\x26\xc8\xb5\x46\x16\x39\x63\x5d\x8e\xdf\x2a\x97\xd4\x8d"
-			"\x0c\x8e\x00\x09\xc8\x17\xf2\xb1\xd3\xd7\xff\x2f\x04\x51\x58\x03"
-			/* hash */
-			/*c52c26c8b5461639635d8edf2a97d48d0c8e0009c817f2b1d3d7ff2f04515803*/
-	};
-	block = calloc(1, sizeof(*block));
-	blockchain = calloc(1, sizeof(*blockchain));
-	if (!blockchain || !block || !list)
+	llist_t *unspent = llist_create(MT_SUPPORT_TRUE);
+
+	if (!chain || !block || !list || !unspent)
 	{
-		free(blockchain), free(block), llist_destroy(list, 1, NULL);
+		free(chain), free(block), llist_destroy(list, 1, NULL);
+		llist_destroy(unspent, 1, NULL);
 		return (NULL);
 	}
-	block->info.timestamp = _genesis.info.timestamp;
-	memcpy(&(block->data.buffer), _genesis.data.buffer, _genesis.data.len);
-	block->data.len = _genesis.data.len;
-	memcpy(&(block->hash), _genesis.hash, SHA256_DIGEST_LENGTH);
+
+	block->info.timestamp = GENESIS_TIMESTAMP;
+	memcpy(&(block->data.buffer), GENESIS_DATA, GENESIS_DATA_LEN);
+	block->data.len = GENESIS_DATA_LEN;
+	memcpy(&(block->hash), GENESIS_HASH, SHA256_DIGEST_LENGTH);
+
 	if (llist_add_node(list, block, ADD_NODE_FRONT))
 	{
-		free(blockchain), free(block), llist_destroy(list, 1, NULL);
+		free(chain), free(block), llist_destroy(list, 1, NULL);
 		return (NULL);
 	}
-	blockchain->chain = list;
-	return (blockchain);
+	chain->chain = list;
+	chain->unspent = unspent;
+	return (chain);
 }
