b12288808165b4c5ef7a69fb990eb73af7b16729
daorejuela1 <daorejuela1@outlook.com>
2022-04-11 02:16:35 +0000

    Added initial steps

--- blockchain/v0.3/transaction/tx_out_create.c
+++ blockchain/v0.3/transaction/tx_out_create.c
@@ -8,17 +8,13 @@
  */
 tx_out_t *tx_out_create(uint32_t amount, uint8_t const pub[EC_PUB_LEN])
 {
-	tx_out_t *trx = calloc(1, sizeof(*trx));
+	tx_out_t *t = calloc(1, sizeof(*t));
 
-	if (!trx)
+	if (!t)
 		return (NULL);
-	trx->amount = amount;
-	memcpy(trx->pub, pub, sizeof(trx->pub));
-	if (!sha256((int8_t const *)trx, sizeof(trx->amount) + sizeof(trx->pub),
-				trx->hash))
-	{
-		free(trx);
-		return (NULL);
-	}
-	return (trx);
+	t->amount = amount;
+	memcpy(t->pub, pub, sizeof(t->pub));
+	if (!sha256((int8_t const *)t, sizeof(t->amount) + sizeof(t->pub), t->hash))
+		return (free(t), NULL);
+	return (t);
 }
